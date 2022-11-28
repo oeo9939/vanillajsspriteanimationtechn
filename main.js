dceb9b35d7e48809d603c4fa295d19cf87ee60c7
Teo <mgttmich@gmail.com>
2022-11-28 20:13:40 +0200

    drawImage(3, 5, 9), add spriteWidth-Height

--- main.js
+++ main.js
@@ -5,13 +5,24 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = ".shadown_dog.png";
-let x = 0;
+playerImage.src = "shadow_dog.png";
+
+// 6876px width - 6876 / 12 columns = 573 (margin, extra pixels)
+const spriteWidth = 575;
+
+// 5230px height - 5230 / 10 rows = 523
+const spriteHeight = 523;
+
+// Showcase it is animated
+// let x = 0;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x, 50, 100, 100);
-    x++;
+    // ctx.fillRect(50, 50, 100, 100);
+    // Showcase it is animated
+    // x++;
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     requestAnimationFrame(animate);
 }
 
