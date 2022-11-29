7ed9468f7527ed22862a1b5fff345f1496f1ae22
Teo <mgttmich@gmail.com>
2022-11-29 22:38:47 +0200

    Add spriteAnimations, animationStates, forEach

--- main.js
+++ main.js
@@ -14,24 +14,47 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 
 let frameX = 0;
-let frameY = 3;
+let frameY = 4;
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 7,
+    },
+    {
+        name: "jump",
+        frames: 7,
+    }
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: []
+    }
+    for (let j = 0; j < state.frames; s++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+    }
+});
 
 // Showcase it is animated
 // let x = 0;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame / staggerFrames) % 11;
+    frameX = spriteWidth * position;
     // ctx.fillRect(50, 50, 100, 100);
     // Showcase it is animated
     // x++;
     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (gameFrame % staggerFrames == 0){
-        if (frameX < 6) frameX++;
-        else frameX = 0;
-    }
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    // if (gameFrame % staggerFrames == 0){
+    //     if (frameX < 6) frameX++;
+    //     else frameX = 0;
+    // }
 
     gameFrame++;
     requestAnimationFrame(animate);
